dbf80bca7273686d56e5428ecfc0807c597277e5
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-12-21 10:38:06 +0100

    remove unused code from schnetkernel lib

--- md17/experiments/schnetkernel/lib.py
+++ md17/experiments/schnetkernel/lib.py
@@ -1,35 +1,18 @@
 import jax
-import jax.numpy as jnp
 import jax.config
 jax.config.update("jax_enable_x64", True)
 import jax.dlpack
 import torch.utils.dlpack
 import torch
 
-import schnetpack as spk
 import schnetpack.properties as properties
 
 import functools
-from functools import partial
 from ase import Atoms
 from ase.neighborlist import neighbor_list
 
 
-# derivative kernel (jax) with nn features (torch) interop example
-
-def make_inner(kappa, xs1, xs2):
-    def kvp(x1, x2, v2):
-        g = lambda x2: jax.grad(kappa)(x1, x2)
-        return jax.jvp(g, (x2,), (v2,))[1]
-    def inner(alpha):
-        return jax.vmap(jax.vmap(kvp, (None, 0, 0)), (0, None, None))(xs1, xs2, alpha)
-    return inner
-
-def make_mvm(kappa, xs1, xs2):
-    inner = make_inner(kappa, xs1, xs2)
-    def mvm(alpha):
-        return jnp.sum(inner(alpha), axis=1)
-    return mvm
+# JAX - PyTorch AD interop
 
 def jax2torch(x):
     return torch.utils.dlpack.from_dlpack(jax.dlpack.to_dlpack(x))
@@ -62,20 +45,6 @@ def torch_vjp(nn, x, v):
     nnv = torch2jax(nnv)
     return nnx, nnv
 
-def make_mvm_nn(kappa, nn, xs1, xs2):
-    nn_xs1 = torch_apply(nn, xs1)
-    nn_xs2 = torch_apply(nn, xs2)
-    @jax.jit # an inner function that doesn't close-over xs (to not capture too much memory with JIT)
-    def _inner_mvm(nn_xs1, nn_xs2, alpha2):
-        return make_mvm(kappa, nn_xs1, nn_xs2)(alpha2)
-    def inner_mvm(alpha2):
-        return _inner_mvm(nn_xs1, nn_xs2, alpha2)
-    def mvm(alpha):
-        nn_xs2, alpha2 = torch_jvp(nn, xs2, alpha)
-        alpha3 = inner_mvm(alpha2)
-        _, alpha4 = torch_vjp(nn, xs1, alpha3)
-        return alpha4
-    return mvm
 
 # Jacobian preaccumulation
   
@@ -95,40 +64,6 @@ def torch_jacobians(nn, x):
     jacs = batch_jacobian(nn, x)
     jacs = torch2jax(jacs)
     return jacs
-
-# Explicit derivative kernelmatrix with preaccumulated Jacobians
-
-@partial(jax.jit, static_argnums=(0,))
-def matrix(kappa, nn_xs1, nn_xs2, jacs1, jacs2):
-    B1, _, _, N1, D1 = jacs1.shape
-    B2, _, _, N2, D2 = jacs2.shape
-    inner = make_inner(kappa, nn_xs1, nn_xs2)
-    KJ = jax.vmap(jax.vmap(inner, (3,)), (3,))(jacs2)
-    JKJ = jnp.einsum("abcde,fgaibc->adeifg", jacs1, KJ) # TODO clean up
-    JKJ = JKJ.reshape(B1*N1*D1, B2*N2*D2)
-    return JKJ
-
-def kernelmatrix_jacobian_preaccumulated(kappa, nn, xs1, xs2, *, jacs1=None, jacs2=None):
-    nn_xs1 = torch_apply(nn, xs1)
-    nn_xs2 = torch_apply(nn, xs2)
-    if jacs1 is None: jacs1 = torch_jacobians(nn, xs1)
-    if jacs2 is None: jacs2 = torch_jacobians(nn, xs2)
-    return matrix(kappa, nn_xs1, nn_xs2, jacs1, jacs2)
-
-@jax.jit
-def psdsolve(K, y, reg):
-    K = K.at[jnp.diag_indices_from(K)].add(reg)
-    alpha = jax.scipy.linalg.solve(K, y, sym_pos=True)
-    return alpha
-
-def mae(y, yp):
-    return jnp.mean(jnp.abs(y - yp))
-
-def batched(f, batch_size):
-    def batched_f(xs):
-        batch_indices = jnp.split(jnp.arange(len(xs)), len(xs) / batch_size)
-        return jnp.vstack([f(xs[idx]) for idx in batch_indices])
-    return batched_f
 
 
 # SchNet inputs
@@ -152,43 +87,3 @@ def schnet_input_dict(r, z, cutoff): # dev-brach schnetpack@83935fb5001cacea1b41
         properties.idx_j: idx_j,
         properties.n_atoms: torch.tensor([n_atoms])
     }
-
-def demo():
-
-    def untrained_schnet(n_features=128, cutoff=5.0):
-        radial_basis = spk.nn.GaussianRBF(n_rbf=20, cutoff=cutoff)
-        representation = spk.representation.SchNet(
-            n_atom_basis=n_features,
-            n_interactions=3,
-            radial_basis=radial_basis,
-            cutoff_fn=spk.nn.CosineCutoff(cutoff),
-        )
-        return representation
-
-        
-    rbf = lambda x1, x2: jnp.exp(-0.5*jnp.sum((x1-x2)**2))
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    cutoff = 5.0
-    schnet = untrained_schnet(cutoff=cutoff).to(device, dtype=torch.float64)
-    z = torch.ones(9, device=device, dtype=torch.int)
-
-    def nn(xs):
-        n_batch, n_atoms, n_dims = xs.shape
-        batch = [schnet_input_dict(x, z, cutoff) for x in xs]
-        batch = spk.data.loader._atoms_collate_fn(batch)
-        return schnet(batch)['scalar_representation'].reshape(n_batch, n_atoms, -1)
-
-    xs1   = torch2jax(torch.randn(5,9,3,device=device, dtype=torch.float64))
-    xs2   = torch2jax(torch.randn(3,9,3,device=device, dtype=torch.float64))
-    alpha = torch2jax(torch.randn(3,9,3,device=device, dtype=torch.float64))
-
-    mvm = make_mvm_nn(rbf, nn, xs1, xs2)
-    mvm_alpha = mvm(alpha)
-    print(mvm_alpha)
-    print(mvm_alpha.shape)
-    print(type(mvm_alpha))
-
-if __name__=='__main__':
-    demo()
